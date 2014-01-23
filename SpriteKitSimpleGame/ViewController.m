2ac5bb69d42d36c4a5ae55c547aaa7ce55e672d8
Michel Mansour <michel.mansour@gmail.com>
2014-01-22 20:46:42 -0500

    replaced viewDidLoad with viewDidLayoutSubviews so the scene size is properly set

--- SpriteKitSimpleGame/ViewController.m
+++ SpriteKitSimpleGame/ViewController.m
@@ -11,21 +11,22 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-
-    // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
     
-    // Create and configure the scene.
-    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+    // Configure the view
+    SKView *skView = (SKView *)self.view;
+    if (!skView.scene) {
+        skView.showsFPS = YES;
+        skView.showsNodeCount = YES;
+        
+        // Create and configure the scene
+        SKScene *scene = [MyScene sceneWithSize:skView.bounds.size];
+        scene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene
+        [skView presentScene:scene];
+    }
 }
 
 - (BOOL)shouldAutorotate
