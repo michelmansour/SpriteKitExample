50d2853756f994d8402c156526023b6c817f91b5
Michel Mansour <michel.mansour@gmail.com>
2014-01-22 21:33:05 -0500

    added sound effects when shooting projectiles

--- SpriteKitSimpleGame/MyScene.m
+++ SpriteKitSimpleGame/MyScene.m
@@ -113,6 +113,9 @@ static inline CGPoint rwNormalize(CGPoint a) {
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
     
+    // Sound effects
+    [self runAction:[SKAction playSoundFileNamed:@"pew-pew-lei.caf" waitForCompletion:NO]];
+    
     // Choose one of the touches to work with
     UITouch *touch = [touches anyObject];
     CGPoint location = [touch locationInNode:self];
