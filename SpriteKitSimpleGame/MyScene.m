fac43bf43c7b854b8a5c9f2cd0c15fcf9370212a
Michel Mansour <michel.mansour@gmail.com>
2014-01-22 21:03:28 -0500

    added vector arithmetic methods

--- SpriteKitSimpleGame/MyScene.m
+++ SpriteKitSimpleGame/MyScene.m
@@ -14,6 +14,28 @@
 @property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 @end
 
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+// Makes a vector have a length of 1
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
 @implementation MyScene
 
 - (id)initWithSize:(CGSize)size {
