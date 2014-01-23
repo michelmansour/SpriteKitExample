aa9d29d03d8a8036dc92b8b542027c84d8006fb6
Michel Mansour <michel.mansour@gmail.com>
2014-01-22 20:25:10 -0500

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Michel Mansour on 1/22/14.
+//  Copyright (c) 2014 Michel Mansour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
